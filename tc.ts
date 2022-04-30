5a6e4168a77b379ae880df1c359d1209c6af660b
padmaja2023 <kollepadmaja23@gmail.com>
2022-04-29 23:46:42 -0700

    check1

--- tc.ts
+++ tc.ts
@@ -166,9 +166,9 @@ export function tcStmt(s: Stmt<any>, classname: string, class_env: ClsEnv,
         });
         s.if_body = if_body;
         const lastIf = if_body[if_body.length - 1]
-        if(lastIf != undefined &&  lastIf.tag === "return" && lastIf.a !== currentReturn) {
-          throw new Error("TYPE ERROR: Incorrect/ null return from branch1")
-        }
+        // if(lastIf != undefined &&  lastIf.tag === "return" && lastIf.a !== currentReturn) {
+          // throw new Error("TYPE ERROR: Incorrect/ null return from branch1")
+        // }
 
         var elif_body: Stmt<any>[] = [];
         s.elif_body.forEach(i => {
