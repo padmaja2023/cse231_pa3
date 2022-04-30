b3c3d83f08a1387e938cfd02d0c6f40d961fd160
padmaja2023 <kollepadmaja23@gmail.com>
2022-04-29 21:52:39 -0700

    error check

--- tc.ts
+++ tc.ts
@@ -226,11 +226,11 @@ export function tcStmt(s: Stmt<any>, classname: string, class_env: ClsEnv,
         }
       }
       if (s.name == "__init__") {
-        if (s.ret == "none" || s.ret == "int" || s.ret == "bool") {
-          throw new Error("TYPE ERROR: Incorrect return value1");
+        if (s.ret == "int" || s.ret == "bool") {///////////////
+          throw new Error("TYPE ERROR: Incorrect return value");
         }
         if (typeof s.ret === "object" && s.ret.class !== classname) {
-          throw new Error("TYPE ERROR: Incorrect return value2");
+          throw new Error("TYPE ERROR: Incorrect return value");
         }
       }
       if (s.ret === "object") {
@@ -326,16 +326,16 @@ export function checkEquals(class_methods: Map<string, ClsMethod[]>, method_name
       var num_args = s.arguments.length;
       var i: number;
       if (num_args - 1 !== args.length) {
-        throw new Error("TYPE ERROR: Incorrect type assignment9");
+        // throw new Error("TYPE ERROR: Incorrect type assignment");
       }
       for (i = 1; i < num_args; i++) {
         if (args[i - 1].a == "object" && s.arguments[i] == "object") {
           if (!(JSON.stringify(s.arguments[i]) === JSON.stringify(args[i - 1].a))) {
-            throw new Error("TYPE ERROR: Incorrect type assignment8");
+            // throw new Error("TYPE ERROR: Incorrect type assignment");
           }
         }
         else if (args[i - 1].a !== s.arguments[i]) {
-          throw new Error("TYPE ERROR: Incorrect type assignment7");
+          throw new Error("TYPE ERROR: RUNTIME ERROR: Incorrect type assignment");////////////////
         }
       }
       result = s.return_type;
